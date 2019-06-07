126234dc6e54e12d6f60497eb0c85e033a501a2b
Colton <colton@coltonbrown.co>
2019-06-07 11:32:17 -0400

    improve social linking

--- components/Layout.js
+++ components/Layout.js
@@ -2,44 +2,55 @@ import Head from 'next/head'
 import Nav from './Nav.js';
 import Footer from './Footer.js';
 
-export default props => (
-  <div>
-    <Head>
-      <script dangerouslySetInnerHTML={{
-        __html: `
-          window.dataLayer = window.dataLayer || [];
-            function gtag(){
-              dataLayer.push(arguments);
-            }
-            gtag('js', new Date());
-            gtag('config', 'UA-93943838-4');
-        `
-      }}/>
-      <meta name="description" content="Multidisciplinary arts festival in Burgundy" />
-      <meta itemprop="name" content="Fey" />
-      <meta itemprop="description" content="Multidisciplinary arts festival in Burgundy" />
-      <meta itemprop="image" content="http://fey-arts.com/images/fey_officiel.png" />
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:site" content="" />
-      <meta name="twitter:title" content="Fey" />
-      <meta name="twitter:description" content="Multidisciplinary arts festival in Burgundy" />
-      <meta name="twitter:image:src" content="http://fey-arts.com/images/fey_officiel.png" />
-      <meta property="og:title" content="Fey" />
-      <meta property="og:image" content="http://fey-arts.com/images/fey_officiel.png" />
-      <meta property="og:description" content="Multidisciplinary arts festival in Burgundy" />
-      <meta property="og:site_name" content="Fey" />
-      <meta property="og:url" content="http://fey-arts.com" />
-      <title>Fey – Rencontres d&#039;arts &#8211; Multidisciplinary artistic festival in Burgundy. 20-22 September 2019</title>
-      <meta charSet="utf-8" />
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
+const social_img_url = 'http://fey-arts.com/static/social.jpg';
+const title = 'Fey — A multidisciplinary arts festival in Bourgogne. 20-22 September 2019';
+const description = 'Multidisciplinary arts festival in Bourgogne';
 
-      <link href="https://fonts.googleapis.com/css?family=Montserrat:400,400i,500,600&display=swap" rel="stylesheet"/> 
-      <link rel="stylesheet" href="/static/style.css" />
-    </Head>
-    <div id="main">
-      <Nav marquees={ props.marquees }/>
-      { props.children }
-    </div>
-    <Footer />
-  </div>
-)
+export default class Home extends React.Component {
+
+  render () {
+    return (
+      <div>
+        <Head>
+          <script dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+                function gtag(){
+                  dataLayer.push(arguments);
+                }
+                gtag('js', new Date());
+                gtag('config', 'UA-93943838-4');
+            `
+          }}/>
+          
+          <meta name="description" content={ description } />
+          <meta itemprop="name" content={ title } />
+          <meta itemprop="description" content={ description } />
+          <meta itemprop="image" content={ social_img_url } />
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:site" content="" />
+          <meta name="twitter:title" content={ title } />
+          <meta name="twitter:description" content={ description } />
+          <meta name="twitter:image:src" content={ social_img_url } />
+          <meta property="og:title" content={ title } />
+          <meta property="og:image" content={ social_img_url } />
+          <meta property="og:description" content={ description } />
+          <meta property="og:site_name" content={ title } />
+          <meta property="og:url" content="https://fey-arts.com" />
+
+          <title>{ title }</title>
+          <meta charSet="utf-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link href="https://fonts.googleapis.com/css?family=Montserrat:400,400i,500,600&display=swap" rel="stylesheet"/>
+          <link rel="stylesheet" href="/static/style.css" />
+
+        </Head>
+        <div id="main">
+          <Nav marquees={ this.props.marquees }/>
+          { this.props.children }
+        </div>
+        <Footer />
+      </div>
+    )
+  }
+}
